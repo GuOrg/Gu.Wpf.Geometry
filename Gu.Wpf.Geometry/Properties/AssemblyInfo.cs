6c930f2c8576075926378678c62d2c4683162ea2
Johan Larsson <johan_gavle1@hotmail.com>
2016-10-13 18:13:40 +0200

    Bump version.

--- Gu.Wpf.Geometry/Properties/AssemblyInfo.cs
+++ Gu.Wpf.Geometry/Properties/AssemblyInfo.cs
@@ -4,9 +4,6 @@ using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Markup;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Gu.Wpf.Geometry")]
 [assembly: AssemblyDescription("Geometries for WPF.")]
 [assembly: AssemblyConfiguration("")]
@@ -15,27 +12,10 @@ using System.Windows.Markup;
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a9cbadf5-65f8-4a81-858c-ea0cdfeb2e99")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
 [assembly: InternalsVisibleTo("Gu.Wpf.Geometry.Tests", AllInternalsVisible = true)]
 [assembly: InternalsVisibleTo("Gu.Wpf.Geometry.Demo", AllInternalsVisible = true)]
 [assembly: InternalsVisibleTo("Gu.Wpf.Geometry.Benchmarks", AllInternalsVisible = true)]

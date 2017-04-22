cd68bc2d1c7a2c73c089843f7387e0b0f870bd37
Johan Larsson <johan_gavle1@hotmail.com>
2017-04-22 16:34:12 +0200

    Add UI-test project with test for all tabs in mainwindow.

---
+++ Gu.Wpf.Geometry.UiTests/Helpers/Info.cs
@@ -0,0 +1,56 @@
+﻿namespace Gu.Wpf.Geometry.UiTests.Helpers
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Reflection;
+
+    using Gu.Wpf.Geometry.Demo;
+
+    using NUnit.Framework;
+
+    public static class Info
+    {
+        public static ProcessStartInfo ProcessStartInfo
+        {
+            get
+            {
+                var assembly = typeof(MainWindow).Assembly;
+                var uri = new Uri(assembly.CodeBase, UriKind.Absolute);
+                var fileName = uri.AbsolutePath;
+                var workingDirectory = Path.GetDirectoryName(fileName);
+                Assert.NotNull(workingDirectory);
+                var processStartInfo = new ProcessStartInfo
+                                           {
+                                               WorkingDirectory = workingDirectory,
+                                               FileName = fileName,
+                                           };
+                return processStartInfo;
+            }
+        }
+
+        internal static ProcessStartInfo CreateStartInfo(string args)
+        {
+            var processStartInfo = new ProcessStartInfo
+                                       {
+                                           FileName = GetExeFileName(),
+                                           Arguments = args,
+                                           UseShellExecute = false,
+                                       };
+            return processStartInfo;
+        }
+
+        private static string GetExeFileName()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var testDirestory = Path.GetDirectoryName(new Uri(assembly.CodeBase).LocalPath);
+            var assemblyName = assembly.GetName().Name;
+            var exeDirectoryName = assemblyName.Replace("UiTests", "Demo");
+            // ReSharper disable once PossibleNullReferenceException
+            var exeDirectory = testDirestory.Replace(assemblyName, exeDirectoryName);
+            var fileName = Path.Combine(exeDirectory, "Gu.Wpf.Geometry.Demo.exe");
+
+            return fileName;
+        }
+    }
+}
---
+++ Gu.Wpf.Geometry.UiTests/MainWindoTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Gu.Wpf.Geometry.UiTests
+{
+    using FlaUI.Core;
+    using FlaUI.Core.Definitions;
+    using FlaUI.UIA3;
+
+    using Gu.Wpf.Geometry.UiTests.Helpers;
+
+    using NUnit.Framework;
+
+    public class MainWindoTests
+    {
+        [Test]
+        public void ClickAllTabs()
+        {
+            // Just a smoke test so that everything builds.
+            using (var app = Application.Launch(Info.ProcessStartInfo))
+            {
+                using (var automation = new UIA3Automation())
+                {
+                    var window = app.GetMainWindow(automation);
+                    var tab = window.FindFirstDescendant(x => x.ByControlType(ControlType.Tab));
+                    foreach (var element in tab.FindAllChildren(x => x.ByControlType(ControlType.TabItem)))
+                    {
+                        var tabItem = element.AsTabItem();
+                        tabItem.Click();
+                    }
+                }
+            }
+        }
+    }
+}

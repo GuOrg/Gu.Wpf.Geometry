45830bd4c380dd83cf01c5e29860d1589d036d1e
Johan Larsson <johan_gavle1@hotmail.com>
2017-04-22 08:07:32 +0200

    Add fade.ps as resource.

--- Gu.Wpf.Geometry/Effects/Fade.cs
+++ Gu.Wpf.Geometry/Effects/Fade.cs
@@ -5,28 +5,24 @@
     using System.Windows.Media;
     using System.Windows.Media.Effects;
 
-
     /// <summary>Fade to a colour by animating the strength.</summary>
     public class Fade : ShaderEffect
     {
         public static readonly DependencyProperty InputProperty = ShaderEffect.RegisterPixelShaderSamplerProperty("Input", typeof(Fade), 0);
-        
+
         public static readonly DependencyProperty StrengthProperty = DependencyProperty.Register("Strength", typeof(double), typeof(Fade), new UIPropertyMetadata(0D, PixelShaderConstantCallback(0)));
-        
+
         public static readonly DependencyProperty ToProperty = DependencyProperty.Register("To", typeof(Color), typeof(Fade), new UIPropertyMetadata(Color.FromArgb(255, 0, 0, 0), PixelShaderConstantCallback(2)));
-        
-        //// <summary>
+
+        /// <summary>
         /// The uri should be something like pack://application:,,,/Gu.Wpf.Geometry;component/Effects/Fade.ps
         /// The file Fade.ps should have BuildAction: Resource
-        //// </summary>
+        /// </summary>
         private static readonly PixelShader Shader = new PixelShader
         {
             UriSource = new Uri("pack://application:,,,/Gu.Wpf.Geometry;component/Effects/Fade.ps", UriKind.Absolute)
         };
-        
-        /// <summary>
-        /// 
-        /// </summary>
+
         public Fade()
         {
             this.PixelShader = Shader;
@@ -34,29 +30,28 @@
             this.UpdateShaderValue(StrengthProperty);
             this.UpdateShaderValue(ToProperty);
         }
-        
-        //// <summary>
+
+        /// <summary>
         /// There has to be a property of type Brush called "Input". This property contains the input image and it is usually not set directly - it is set automatically when our effect is applied to a control.
-        //// </summary>
+        /// </summary>
         public Brush Input
         {
-            get => ((Brush)(this.GetValue(InputProperty)));
+            get => (Brush)this.GetValue(InputProperty);
             set => this.SetValue(InputProperty, value);
         }
-        
+
         /// <summary>The color used to tint the input.</summary>
         public double Strength
         {
-            get => ((double)(this.GetValue(StrengthProperty)));
+            get => (double)this.GetValue(StrengthProperty);
             set => this.SetValue(StrengthProperty, value);
         }
-        
+
         /// <summary>The colour to fade to.</summary>
         public Color To
         {
-            get => ((Color)(this.GetValue(ToProperty)));
+            get => (Color)this.GetValue(ToProperty);
             set => this.SetValue(ToProperty, value);
         }
     }
 }
-

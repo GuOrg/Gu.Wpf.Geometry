02cc26576abd390990cbe10f5af5aa0516a17f84
Johan Larsson <johan_gavle1@hotmail.com>
2017-05-27 18:47:22 +0200

    Zoombox WIP.

---
+++ Gu.Wpf.Geometry/Internals/EmptyEnumerator.cs
@@ -0,0 +1,25 @@
+namespace Gu.Wpf.Geometry
+{
+    using System;
+    using System.Collections;
+
+    internal class EmptyEnumerator : IEnumerator
+    {
+        public static readonly IEnumerator Instance = new EmptyEnumerator();
+
+        private EmptyEnumerator()
+        {
+        }
+
+        object IEnumerator.Current => throw new InvalidOperationException();
+
+        void IEnumerator.Reset()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            return false;
+        }
+    }
+}
---
+++ Gu.Wpf.Geometry/Internals/SingleChildEnumerator.cs
@@ -0,0 +1,31 @@
+namespace Gu.Wpf.Geometry
+{
+    using System.Collections;
+
+    internal class SingleChildEnumerator : IEnumerator
+    {
+        private readonly int count;
+        private readonly object child;
+
+        private int index = -1;
+
+        internal SingleChildEnumerator(object child)
+        {
+            this.child = child;
+            this.count = child == null ? 0 : 1;
+        }
+
+        object IEnumerator.Current => (this.index == 0) ? this.child : null;
+
+        bool IEnumerator.MoveNext()
+        {
+            this.index++;
+            return this.index < this.count;
+        }
+
+        void IEnumerator.Reset()
+        {
+            this.index = -1;
+        }
+    }
+}

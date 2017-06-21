b8ffa88ad053da25f52b83627d63a5d2e5c3b367
JohanLarsson <johan_gavle1@hotmail.com>
2017-06-21 15:18:18 +0200

    Cleanup.

--- Gu.Wpf.Geometry.UiTests/Helpers/Info.cs
+++ Gu.Wpf.Geometry.UiTests/Helpers/Info.cs
@@ -5,25 +5,15 @@
     using System.IO;
     using System.Reflection;
 
-    using Gu.Wpf.Geometry.Demo;
-
-    using NUnit.Framework;
-
     public static class Info
     {
         public static ProcessStartInfo ProcessStartInfo
         {
             get
             {
-                var assembly = typeof(MainWindow).Assembly;
-                var uri = new Uri(assembly.CodeBase, UriKind.Absolute);
-                var fileName = uri.AbsolutePath;
-                var workingDirectory = Path.GetDirectoryName(fileName);
-                Assert.NotNull(workingDirectory);
                 var processStartInfo = new ProcessStartInfo
                 {
-                    WorkingDirectory = workingDirectory,
-                    FileName = fileName,
+                    FileName = GetExeFileName(),
                 };
                 return processStartInfo;
             }
@@ -43,11 +33,11 @@
         private static string GetExeFileName()
         {
             var assembly = Assembly.GetExecutingAssembly();
-            var testDirestory = Path.GetDirectoryName(new Uri(assembly.CodeBase).LocalPath);
+            var testDirectory = Path.GetDirectoryName(new Uri(assembly.CodeBase).LocalPath);
             var assemblyName = assembly.GetName().Name;
             var exeDirectoryName = assemblyName.Replace("UiTests", "Demo");
             //// ReSharper disable once PossibleNullReferenceException
-            var exeDirectory = testDirestory.Replace(assemblyName, exeDirectoryName);
+            var exeDirectory = testDirectory.Replace(assemblyName, exeDirectoryName);
             var fileName = Path.Combine(exeDirectory, "Gu.Wpf.Geometry.Demo.exe");
             return fileName;
         }

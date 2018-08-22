043b04cda9cc6a8d2488fa96c059876901b7063c
JohanLarsson <johan_gavle1@hotmail.com>
2018-08-22 08:54:40 +0200

    Cleanup ZoomCommands

--- Gu.Wpf.Geometry/ZoomCommands.cs
+++ Gu.Wpf.Geometry/ZoomCommands.cs
@@ -1,6 +1,5 @@
 namespace Gu.Wpf.Geometry
 {
-    using System.Runtime.CompilerServices;
     using System.Windows.Input;
 
     /// <summary>
@@ -8,33 +7,29 @@ namespace Gu.Wpf.Geometry
     /// </summary>
     public static class ZoomCommands
     {
-        /////// <summary>
-        /////// The content is resized to fill the destination dimensions. The aspect ratio is not preserved.
-        /////// </summary>
-        ////public static RoutedUICommand Fill { get; } = Create();
-
         /// <summary>
         /// The content preserves its original size.
         /// </summary>
-        public static RoutedUICommand None { get; } = Create();
+        public static RoutedUICommand None { get; } = new RoutedUICommand(nameof(None), nameof(None), typeof(ZoomCommands));
 
         /// <summary>
-        /// The content is resized to fit in the destination dimensions while it preserves its native aspect ratio.
+        /// The content is re-sized to fit in the destination dimensions while it preserves its native aspect ratio.
         /// </summary>
-        public static RoutedUICommand Uniform { get; } = Create();
+        public static RoutedUICommand Uniform { get; } = new RoutedUICommand(nameof(Uniform), nameof(Uniform), typeof(ZoomCommands));
 
         /// <summary>
-        /// The content is resized to fill the destination dimensions while it preserves its native aspect ratio. If the aspect ratio of the destination rectangle differs from the source, the source content is clipped to fit in the destination dimensions.
+        /// The content is re-sized to fill the destination dimensions while it preserves its native aspect ratio. If the aspect ratio of the destination rectangle differs from the source, the source content is clipped to fit in the destination dimensions.
         /// </summary>
-        public static RoutedUICommand UniformToFill { get; } = Create();
+        public static RoutedUICommand UniformToFill { get; } = new RoutedUICommand(nameof(UniformToFill), nameof(UniformToFill), typeof(ZoomCommands));
 
-        public static RoutedUICommand Increase { get; } = Create();
+        /// <summary>
+        /// Increase the zoom.
+        /// </summary>
+        public static RoutedUICommand Increase { get; } = new RoutedUICommand(nameof(Increase), nameof(Increase), typeof(ZoomCommands));
 
-        public static RoutedUICommand Decrease { get; } = Create();
-
-        private static RoutedUICommand Create([CallerMemberName] string name = null)
-        {
-            return new RoutedUICommand(name, name, typeof(ZoomCommands));
-        }
+        /// <summary>
+        /// Decrease the zoom.
+        /// </summary>
+        public static RoutedUICommand Decrease { get; } = new RoutedUICommand(nameof(Decrease), nameof(Decrease), typeof(ZoomCommands));
     }
 }

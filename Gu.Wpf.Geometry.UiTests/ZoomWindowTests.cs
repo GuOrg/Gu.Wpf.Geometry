4fec60a09e7be0c0b9330cbe411e3e43a67dbb8e
JohanLarsson <johan_gavle1@hotmail.com>
2019-12-09 08:52:05 +0100

    Try more waits.

--- Gu.Wpf.Geometry.UiTests/ZoomWindowTests.cs
+++ Gu.Wpf.Geometry.UiTests/ZoomWindowTests.cs
@@ -15,7 +15,8 @@ namespace Gu.Wpf.Geometry.UiTests
         {
             using var app = Application.AttachOrLaunch("Gu.Wpf.Geometry.Demo.exe", WindowName);
             var window = app.MainWindow;
-            window.FindButton("None").Click();
+            window.FindButton("None").Invoke();
+            app.WaitWhileBusy();
         }
 
         [OneTimeTearDown]
@@ -35,15 +36,18 @@ namespace Gu.Wpf.Geometry.UiTests
             var zoomButton = window.FindButton("Uniform");
             Assert.AreEqual(true, zoomButton.IsEnabled);
             zoomButton.Click();
+            app.WaitWhileBusy();
             Assert.AreEqual("386, 248", renderSize.Text);
             Assert.AreEqual("0.62,0,0,0.62,100,0", contentMatrix.Text);
 
             window.FindButton("None").Click();
+            app.WaitWhileBusy();
             Assert.AreEqual("386, 248", renderSize.Text);
             Assert.AreEqual("Identity", contentMatrix.Text);
 
             Assert.AreEqual(true, zoomButton.IsEnabled);
             zoomButton.Click();
+            app.WaitWhileBusy();
             Assert.AreEqual("386, 248", renderSize.Text);
             Assert.AreEqual("0.62,0,0,0.62,100,0", contentMatrix.Text);
         }
@@ -59,15 +63,18 @@ namespace Gu.Wpf.Geometry.UiTests
 
             Assert.AreEqual(true, zoomButton.IsEnabled);
             zoomButton.Click();
+            app.WaitWhileBusy();
             Assert.AreEqual("386, 248", renderSize.Text);
             Assert.AreEqual("1.28667,0,0,1.28667,0,-133.33333", contentMatrix.Text);
 
             window.FindButton("None").Click();
+            app.WaitWhileBusy();
             Assert.AreEqual("386, 248", renderSize.Text);
             Assert.AreEqual("Identity", contentMatrix.Text);
 
             Assert.AreEqual(true, zoomButton.IsEnabled);
             zoomButton.Click();
+            app.WaitWhileBusy();
             Assert.AreEqual("386, 248", renderSize.Text);
             Assert.AreEqual("1.28667,0,0,1.28667,0,-133.33333", contentMatrix.Text);
         }
@@ -80,15 +87,18 @@ namespace Gu.Wpf.Geometry.UiTests
             var renderSize = window.FindTextBlock("Size");
             var contentMatrix = window.FindTextBlock("ContentMatrix");
             window.FindButton("Uniform").Invoke();
+            app.WaitWhileBusy();
             var zoomButton = window.FindButton("Increase");
 
             Assert.AreEqual(true, zoomButton.IsEnabled);
             zoomButton.Click();
+            app.WaitWhileBusy();
             Assert.AreEqual("386, 248", renderSize.Text);
             Assert.AreEqual("1.24,0,0,1.24,7,-124", contentMatrix.Text);
 
             Assert.AreEqual(true, zoomButton.IsEnabled);
             zoomButton.Click();
+            app.WaitWhileBusy();
             Assert.AreEqual("386, 248", renderSize.Text);
             Assert.AreEqual("2.48,0,0,2.48,-179,-372", contentMatrix.Text);
         }
@@ -101,15 +111,18 @@ namespace Gu.Wpf.Geometry.UiTests
             var renderSize = window.FindTextBlock("Size");
             var contentMatrix = window.FindTextBlock("ContentMatrix");
             window.FindButton("Uniform").Invoke();
+            app.WaitWhileBusy();
             var zoomButton = window.FindButton("Decrease");
 
             Assert.AreEqual(true, zoomButton.IsEnabled);
             zoomButton.Click();
+            app.WaitWhileBusy();
             Assert.AreEqual("386, 248", renderSize.Text);
             Assert.AreEqual("0.31,0,0,0.31,146.5,62", contentMatrix.Text);
 
             Assert.AreEqual(true, zoomButton.IsEnabled);
             zoomButton.Click();
+            app.WaitWhileBusy();
             Assert.AreEqual("386, 248", renderSize.Text);
             Assert.AreEqual("0.155,0,0,0.155,169.75,93", contentMatrix.Text);
         }
@@ -123,16 +136,17 @@ namespace Gu.Wpf.Geometry.UiTests
             var contentMatrix = window.FindTextBlock("ContentMatrix");
             var zoomBox = window.FindGroupBox("Zoombox");
             window.FindButton("None").Invoke();
+            app.WaitWhileBusy();
             Assert.AreEqual("386, 248", renderSize.Text);
             Assert.AreEqual("Identity", contentMatrix.Text);
 
             Mouse.DragHorizontally(MouseButton.Left, zoomBox.Bounds.Center(), 50);
-            window.WaitUntilResponsive();
+            app.WaitWhileBusy();
             Assert.AreEqual("386, 248", renderSize.Text);
             Assert.AreEqual("1,0,0,1,50,0", contentMatrix.Text);
 
             Mouse.DragHorizontally(MouseButton.Left, zoomBox.Bounds.Center(), -50);
-            window.WaitUntilResponsive();
+            app.WaitWhileBusy();
             Assert.AreEqual("386, 248", renderSize.Text);
             Assert.AreEqual("Identity", contentMatrix.Text);
 

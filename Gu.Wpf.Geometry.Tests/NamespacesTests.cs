647aa345f7547f66c19cf7ece10cba5e08d17218
Johan Larsson <johan_gavle1@hotmail.com>
2020-04-27 18:32:41 +0200

    Remove test that is checked by analyzer.

--- Gu.Wpf.Geometry.Tests/NamespacesTests.cs
+++ Gu.Wpf.Geometry.Tests/NamespacesTests.cs
@@ -19,33 +19,6 @@ namespace Gu.Wpf.Geometry.Tests
         }
 
         [Test]
-        public void XmlnsDefinitions()
-        {
-            string[] skip = { ".Annotations", ".Properties", "XamlGeneratedNamespace" };
-
-            var strings = this.assembly.GetTypes()
-                                   .Select(x => x.Namespace)
-                                   .Distinct()
-                                   .Where(x => x != null && !skip.Any(x.EndsWith))
-                                   .OrderBy(x => x)
-                                   .ToArray();
-            var attributes = this.assembly.CustomAttributes.Where(x => x.AttributeType == typeof(XmlnsDefinitionAttribute))
-                                      .ToArray();
-            var actuals = attributes.Select(a => a.ConstructorArguments[1].Value)
-                                    .OrderBy(x => x);
-            foreach (var s in strings)
-            {
-                Console.WriteLine(@"[assembly: XmlnsDefinition(""{0}"", ""{1}"")]", Uri, s);
-            }
-
-            Assert.AreEqual(strings, actuals);
-            foreach (var attribute in attributes)
-            {
-                Assert.AreEqual(Uri, attribute.ConstructorArguments[0].Value);
-            }
-        }
-
-        [Test]
         public void XmlnsPrefix()
         {
             var attributes = this.assembly.CustomAttributes.Where(x => x.AttributeType == typeof(XmlnsPrefixAttribute));
